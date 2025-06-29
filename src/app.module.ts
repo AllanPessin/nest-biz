ab7e84b0802f1862ddb7a3a4bc909224bbaf28ff
AllanPessin <allan.pessin@outlook.com>
2025-06-28 22:33:02 -0300

    feat: add @nestjs/config for environment configuration and enable CORS in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,17 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CompaniesModule } from './companies/companies.module';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, CompaniesModule, ProductsModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    PrismaModule,
+    CompaniesModule,
+    ProductsModule,
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
